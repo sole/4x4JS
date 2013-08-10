34eee6c7bc5da206d3bbc861925c930cc700956d
Soledad Penades <listas@soledadpenades.com>
2013-08-10 12:05:28 +0100

    browserifying

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+function init() {
+	console.log('app init, yo!');
+}
+
+module.exports = {
+	init: init
+};
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
 window.addEventListener('DOMComponentsLoaded', function() {
+
+	var app = require('./app');
+
 	console.log('hey');
+
+	app.init();
+
 }, false);
