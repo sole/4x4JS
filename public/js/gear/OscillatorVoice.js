ab2a0616ac873fad2a3fef6c5e7a2738b58af11b
Soledad Penades <listas@soledadpenades.com>
2013-08-14 22:47:52 +0100

    better add oscillator voices and encapsulate the "respawning" there...

---
+++ public/js/gear/OscillatorVoice.js
@@ -0,0 +1,12 @@
+function OscillatorVoice(context) {
+
+	var internalOscillator = null;
+
+	this.noteOn = function(frequency, when) {
+	};
+
+	this.noteOff = function(when) {
+	};
+}
+
+module.exports = OscillatorVoice;
