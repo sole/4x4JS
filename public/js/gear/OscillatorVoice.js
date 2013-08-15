cc6211c1b7ea2d303c8c7005efed3fbf8976ce9c
Soledad Penades <listas@soledadpenades.com>
2013-08-15 08:32:57 +0100

    OscillatorVoice -> add portamento config option

--- public/js/gear/OscillatorVoice.js
+++ public/js/gear/OscillatorVoice.js
@@ -1,17 +1,28 @@
-function OscillatorVoice(context /* TODO options */) {
+function OscillatorVoice(context, options) {
 
 	var internalOscillator = null;
 	var gain = context.createGain();
 
+	options = options || {};
+
+	var portamento = options.portamento !== undefined ? options.portamento : true;
+
 	this.output = gain;
 
 	this.noteOn = function(frequency, when) {
+
+		if(!portamento) {
+			this.noteOff(0);
+		}
+
 		if(internalOscillator === null) {
 			internalOscillator = context.createOscillator();
 			internalOscillator.connect(gain);
 		}
+		
 		internalOscillator.frequency.value = frequency;
 		internalOscillator.start(when);
+
 	};
 
 	this.noteOff = function(when) {
