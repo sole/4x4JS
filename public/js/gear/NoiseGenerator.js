1bee2c76817e0f83e466ddb239bf0484096723fa
Soledad Penades <listas@soledadpenades.com>
2013-08-19 08:42:26 +0100

    begin with noise generator unit

---
+++ public/js/gear/NoiseGenerator.js
@@ -0,0 +1,100 @@
+
+function generateWhiteNoise(size) {
+
+	var out = [];
+	for(var i = 0; i < size; i++) {
+		out.push(Math.random() * 2 - 1);
+	}
+	return out;
+
+}
+
+// Pink and brown noise generation algorithms adapted from
+// https://github.com/zacharydenton/noise.js/blob/master/noise.js
+
+function generatePinkNoise(size) {
+
+	var out = generateWhiteNoise(size);
+	var b0, b1, b2, b3, b4, b5, b6;
+	
+	b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+
+	for (var i = 0; i < size; i++) {
+
+		var white = out[i];
+
+		b0 = 0.99886 * b0 + white * 0.0555179;
+		b1 = 0.99332 * b1 + white * 0.0750759;
+		b2 = 0.96900 * b2 + white * 0.1538520;
+		b3 = 0.86650 * b3 + white * 0.3104856;
+		b4 = 0.55000 * b4 + white * 0.5329522;
+		b5 = -0.7616 * b5 - white * 0.0168980;
+		out[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+		out[i] *= 0.11; // (roughly) compensate for gain
+		b6 = white * 0.115926;
+
+	}
+
+	return out;
+}
+
+function generateBrownNoise(size) {
+
+	var out = generateWhiteNoise(size);
+	var lastOutput = 0.0;
+
+	for(var i = 0; i < size; i++) {
+
+		var white = out[i];
+		out[i] = (lastOutput + (0.02 * white)) / 1.02;
+		lastOutput = out[i];
+		out[i] *= 3.5; // (roughly) compensate for gain
+		
+	}
+
+	return out;
+
+}
+
+function NoiseGenerator(audioContext, options) {
+	
+	options = options || {};
+	
+	var type = options.type || 'white';
+	var length = options.length || 4096;
+
+	buildBuffer(length, type);
+
+	// TODO output, input, regenerate on noteoff, noteon
+
+	// 
+	
+	function buildBuffer(length, type) {
+
+		var noiseFunction, bufferData;
+
+		switch(type) {
+			
+			case 'pink': noiseFunction = generatePinkNoise;
+					break;
+
+			case 'brown': noiseFunction = generateBrownNoise;
+					break;
+
+			default:
+			case 'white': noiseFunction = generateWhiteNoise;
+					break;
+		}
+
+		bufferData = noiseFunction(length);
+
+		var buffer = audioContext.createBuffer(1, length, audioContext.samplingRate);
+		var source = audioContext.createBufferSource();
+
+		source.loop = true;
+		source.buffer = buffer;
+
+	}
+}
+
+module.exports = NoiseGenerator;
